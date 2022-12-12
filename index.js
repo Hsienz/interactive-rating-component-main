ed6ae7aac9a6f5d98ea0fda5429ab4b0439664d7
Hsienz <hsieneis@gmail.com>
2022-12-12 15:26:40 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let prevElem;
+let prevBackgroundColor;
+for( let i = 1; i <= 5; i ++ ) {
+    console.log(1)
+    console.log(`ratingBtn${i}`)
+    const elem = document.getElementById(`ratingBtn${i}`)
+    console.log(elem)
+    elem.addEventListener('click', (e) => {
+        if( prevElem ) {
+            prevElem.style.backgroundColor = prevBackgroundColor;
+        }
+        prevBackgroundColor = elem.style.backgroundColor
+        elem.style.backgroundColor = window.getComputedStyle(document.documentElement).getPropertyValue('--Orange');
+        prevElem = elem;
+    })
+}
+
+let elem = document.getElementById('ratingSubmitBtn');
+elem.addEventListener('click', (e)=>{
+    if( !prevElem ) {
+        alert('Rate first!')
+    }
+    let page = document.getElementById('ratingPage')
+    page.style.display = 'none'
+    page = document.getElementById('thankPage')
+    page.style.display = 'flex'
+    document.getElementById('ratingSpan').innerHTML = prevElem.textContent
+})
